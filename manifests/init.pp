3cc7978b4e4cbd3ab679beeee40cd41ae0eb91bc
Nassim Ait Boufrad <nassim.aitboufrad@francetv.fr>
2013-12-17 19:09:46 +0100

    update to puppet > 2.7

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,35 @@
-# keepalived/manifests/init.pp
-# Benoit Cattie <puppet@benoit.cattie.net>
+# Class: keepalived::server
+#
+# This class manage keepalived server installation and configuration. 
+#
+class keepalived::server {
 
-import "defines/*.pp"
-import "classes/*.pp"
+	package { keepalived: ensure => installed }
 
+	service { keepalived:
+		ensure => running,
+		enable => true,
+		require => Package["keepalived"],
+	}
+
+    concatenated_file { "/etc/keepalived/keepalived.conf": 
+        dir => "/var/lib/puppet/modules/keepalived/real_servers", 
+        header => "/etc/keepalived/PP_keepalived.conf.header", 
+        require => Package["keepalived"], 
+        before => Exec["reload-keepalived"], 
+    }
+
+    file {"/etc/keepalived/PP_keepalived.conf.header":
+        content => template("keepalived/keepalived.conf.header.erb"),
+        mode => 0644, owner => root, group => 0,
+        before => Concatenated_file["/etc/keepalived/keepalived.conf"],
+        notify => Exec["reload-keepalived"],
+    }
+
+	exec{"reload-keepalived":
+		command => "/etc/init.d/keepalived reload",
+        refreshonly => true,
+    }
+
+	file{"/var/lib/puppet/modules/keepalived": ensure => directory}
+}
