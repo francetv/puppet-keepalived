8f50899e007f530926a78e3e2dff2f1d8ea31be4
Nassim Ait Boufrad <nassim.aitboufrad@francetv.fr>
2013-12-19 14:13:12 +0100

    correct

--- manifests/virtual_server.pp
+++ manifests/virtual_server.pp
@@ -49,20 +49,22 @@ define keepalived::virtual_server (
 	# Be carefull when server reboots
 
 	if $state == "MASTER" { #Export only when MASTER
-		exec{"add-loopback-DSR-$virtual_ipaddress":
-			command => "/sbin/ip addr add ${virtual_ipaddress}/32 dev lo",
-			onlyif => "/usr/bin/test -z \"`/sbin/ip addr ls lo | grep ${virtual_ipaddress}/32`\"",
-			tag => "keepalived-exported-dsr-config-$name",
-		}
-		exec{"add-arp_announce-config-DSR-$name":
-			command => "/sbin/sysctl net.ipv4.conf.all.arp_announce=2",
-			onlyif => "/usr/bin/test -z \"`/sbin/sysctl net.ipv4.conf.all.arp_announce | grep 2`\"",
-			tag => "keepalived-exported-dsr-config-$name",
-		}
-		exec{"add-arp_ignore-config-DSR-$name":
-			command => "/sbin/sysctl net.ipv4.conf.all.arp_ignore=1",
-			onlyif => "/usr/bin/test -z \"`/sbin/sysctl net.ipv4.conf.all.arp_ignore | grep 1`\"",
-			tag => "keepalived-exported-dsr-config-$name",
+		$virtual_ipaddress.foreach { |$value|
+			exec{"add-loopback-DSR-$value":
+				command => "/sbin/ip addr add ${value}/32 dev lo",
+				onlyif => "/usr/bin/test -z \"`/sbin/ip addr ls lo | grep ${value}/32`\"",
+				tag => "keepalived-exported-dsr-config-$name",
+			}
+			exec{"add-arp_announce-config-DSR-$name":
+				command => "/sbin/sysctl net.ipv4.conf.all.arp_announce=2",
+				onlyif => "/usr/bin/test -z \"`/sbin/sysctl net.ipv4.conf.all.arp_announce | grep 2`\"",
+				tag => "keepalived-exported-dsr-config-$name",
+			}
+			exec{"add-arp_ignore-config-DSR-$name":
+				command => "/sbin/sysctl net.ipv4.conf.all.arp_ignore=1",
+				onlyif => "/usr/bin/test -z \"`/sbin/sysctl net.ipv4.conf.all.arp_ignore | grep 1`\"",
+				tag => "keepalived-exported-dsr-config-$name",
+			}
 		}
 	}
  
