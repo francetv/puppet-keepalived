63c146e86e5de59fffa3b0694e4e8873c0fdaed2
Nassim Ait boufrad <nassim.aitboufrad@francetv.fr>
2014-07-18 11:29:49 +0200

    maj ha_script.sh

--- files/etc/keepalived/ha_script.sh
+++ files/etc/keepalived/ha_script.sh
@@ -3,33 +3,36 @@
 service=$1
 ip=$2
 port=$3
+id_server=$4
 
 if [ -f "/var/run/ha_script.$service.lock" ]; then
-	exit 1
+        if [ $id_server -eq 1 ]; then
+                exit 1
+        fi
 fi
 
 /bin/nc -z $ip $port
 if [ $? -ne 0 ]; then
-	touch /var/run/ha_script.$service.lock
-	exit 1
+        touch /var/run/ha_script.$service.lock
+        exit 1
 fi
 
 case $service in
-	redis*)
-		/usr/bin/redis-cli INFO | grep role | cut -d : -f2 | grep 'master'
-		if [ $? -eq 0 ]; then
-			# This server is master
-			exit 0
-		else
-			# This server is not master
-			touch /var/run/ha_script.$service.lock
-			exit 1
-		fi
-	;;
-	"mysql")
-		exit 0
-	;;
-	*)
-		exit 0
-	;;
+        redis*)
+                /usr/bin/redis-cli INFO | grep role | cut -d : -f2 | grep 'master'
+                if [ $? -eq 0 ]; then
+                        # This server is master
+                        exit 0
+                else
+                        # This server is not master
+                        touch /var/run/ha_script.$service.lock
+                        exit 1
+                fi
+        ;;
+        "mysql")
+                exit 0
+        ;;
+        *)
+                exit 0
+        ;;
 esac
