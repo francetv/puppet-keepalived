3b74af8d62a01acf6cf77f51b0e55417697d1b0b
Nassim Ait boufrad <nassim.aitboufrad@francetv.fr>
2015-03-29 16:51:36 +0200

    exec to augeas for tcp tuning + tcp nonlocal bind

--- manifests/ha.pp
+++ manifests/ha.pp
@@ -16,12 +16,12 @@ define keepalived::ha (
 		notify => Exec["reload-keepalived"],
 	}
 
-	exec{"add-arp_announce-config-DSR":
-		command => "/sbin/sysctl net.ipv4.conf.all.arp_announce=2",
-		onlyif => "/usr/bin/test -z \"`/sbin/sysctl net.ipv4.conf.all.arp_announce | grep 2`\"",
-	}
-	exec{"add-arp_ignore-config-DSR":
-		command => "/sbin/sysctl net.ipv4.conf.all.arp_ignore=1",
-		onlyif => "/usr/bin/test -z \"`/sbin/sysctl net.ipv4.conf.all.arp_ignore | grep 1`\"",
+	augeas{"tcp tunning":
+		context => "/files/etc/sysctl.conf",
+		changes => [
+			"set net.ipv4.conf.all.arp_announce	2",
+			"set net.ipv4.conf.all.arp_ignore		1",
+			"set net.ipv4.ip_nonlocal_bind			1",
+		],
 	}
 }
